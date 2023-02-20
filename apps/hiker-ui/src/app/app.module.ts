8996c0bf4571ff78712e5cdd1f17cd97ccfa9ffc
László Csatlós <plutoz01@gmail.com>
2023-02-20 15:04:44 +0100

    install ngrx and register as root dependency

--- apps/hiker-ui/src/app/app.module.ts
+++ apps/hiker-ui/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { DataTrailModule } from '@trail-track-r/data/trail';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,9 +16,22 @@ import { DataTrailModule } from '@trail-track-r/data/trail';
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    DataTrailModule
+    DataTrailModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
