17dba26abea4c04ec5d33128546d5172dec53420
László Csatlós <plutoz01@gmail.com>
2023-04-04 15:11:33 +0200

    Remove old schema based libs

--- libs/ui/feature/trails/src/lib/+state/trail-segment-group/trail-segment-group-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TrailSegmentGroupDto } from '@trail-track-r/api-contract/trail';
-
-export const createTrailSegmentGroupDto = (id: string, trailId = '', name = ''): TrailSegmentGroupDto => ({
-  id,
-  trailId,
-  name: name || `name-${id}`
-});
--- libs/ui/feature/trails/src/lib/+state/trail/trails.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { TrailDto } from '@trail-track-r/api-contract/trail';
-
-export const initTrails = createAction('[Trails Page] Init');
-
-export const loadTrailsSuccess = createAction('[Trails/API] Load Trails Success', props<{ trails: TrailDto[] }>());
-
-export const loadTrailsFailure = createAction('[Trails/API] Load Trails Failure', props<{ error: unknown }>());
