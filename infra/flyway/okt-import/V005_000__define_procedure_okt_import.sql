a9c773cb93c06502f54741bcf029eff70b2a4096
László Csatlós <plutoz01@gmail.com>
2023-04-18 13:54:41 +0200

    Add geometry simplification + URL for OKT trail and segment group

--- infra/flyway/okt-import/V005_000__define_procedure_okt_import.sql
+++ infra/flyway/okt-import/V005_000__define_procedure_okt_import.sql
@@ -6,6 +6,7 @@ AS $$
 DECLARE
 	OKT_NAME CONSTANT TEXT := 'Országos Kéktúra (OKT)';
 	okt_trail_id UUID;
+	GEOM_SIMPLIFY_TOLERANCE CONSTANT FLOAT := 0.0001;
 BEGIN
 	IF truncate_target THEN
 	  RAISE info 'truncating target tables...';
@@ -18,23 +19,28 @@ BEGIN
 		CASCADE;
 	END IF;
 
+
 	RAISE info 'inserting data to "trails" schema...';
 	INSERT INTO trails.trail (name, properties)
 	VALUES (OKT_NAME, jsonb_build_object(
-    'length', (SELECT round(SUM(ST_Length(geom::geography))::numeric, 2) FROM okt_import.track)
-  ));
+			'length', (SELECT round(SUM(ST_Length(geom::geography))::numeric, 2) FROM okt_import.track),
+			'url', 'https://www.kektura.hu'
+		));
 
 	okt_trail_id := ( SELECT id FROM trails.trail WHERE name = OKT_NAME);
 	RAISE info 'okt trail id: %', okt_trail_id;
+
 
 	INSERT INTO trails.trail_segment_group (name, trail_id, properties)
 	SELECT
 		name,
 		okt_trail_id,
 		jsonb_build_object(
-			'length', round(ST_Length(geom::geography)::numeric, 2)
+			'length', round(ST_Length(geom::geography)::numeric, 2),
+			'url', 'https://www.kektura.hu/okt-szakasz/okt-' || LPAD(id::TEXT, 2, '0')
 		) AS properties
 	FROM okt_import.track;
+
 
 	INSERT INTO trails.trail_segment (name, trail_segment_group_id, properties, path)
 	SELECT
@@ -43,7 +49,7 @@ BEGIN
 		jsonb_build_object(
 			'length', round(ST_Length(t.geom::geography)::numeric, 2)
 		) AS properties,
-		t.geom AS path
+		ST_Simplify(t.geom, GEOM_SIMPLIFY_TOLERANCE) AS path
 	FROM okt_import.track_splitted t
 	LEFT OUTER JOIN trails.trail_segment_group sg ON t.name = sg.name;
 
